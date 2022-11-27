56aa4a8534b511db02092cc61f1e80a23ccfb9e3
2pokek <alkuzmich6479@gmail.com>
2022-11-28 01:39:43 +0300

    final

--- photos.py
+++ photos.py
@@ -1,8 +1,20 @@
-import os
+import os , shutil
 os.chdir('name')
-for i in os.listdir():
+counter= 0
+for dir in os.listdir():
+    print(counter, dir)
     try:
         os.chdir(dir)
+        for file in os.listdir():
+            if file.endswith('_1.jpg'):
+                collection =  file[3:5]
+                collection_dir = '../../new photos'+'/'+collection
+
+                if not os.path.exists(collection_dir):
+                    os.mkdir(collection_dir)
+
+                shutil.copy2(file , 'collection_dir')
         os.chdir('..')
+        counter += 1
     except notADirectoryError:
         continue
